f10cff229cf4b211b0536eb067caf2abf3bbe4ae
Brian Gamble <brianjgamble@users.noreply.github.com>
2020-02-19 23:12:02 -0600

    The game loop

---
+++ src/game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_GAME_H
+#define GAME_GAME_H
+
+#include <SDL.h>
+
+class Game {
+  public:
+    Game();
+    ~Game();
+
+    void initialize(int width, int height);
+    bool isRunning() const;
+    void processInput();
+    void update();
+    void render();
+    void destroy();
+
+  private:
+    bool running;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "constants.h"
+#include "game.h"
 
 int main() {
-    std::cout << "Game is running..." << std::endl;
+    Game game = Game {};
+
+    game.initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while (game.isRunning()) {
+        game.processInput();
+        game.update();
+        game.render();
+    }
+
+    game.destroy();
+
     return 0;
 }
