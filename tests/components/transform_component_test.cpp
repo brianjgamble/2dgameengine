69d02370df59feb59b9613df00b0adda95effe2e
Brian Gamble <brianjgamble@users.noreply.github.com>
2020-03-09 23:55:11 -0500

    Oops, missing a few files...

---
+++ tests/components/transform_component_test.cpp
@@ -0,0 +1,35 @@
+#include "../../src/components/transform_component.h"
+#include <catch.hpp>
+
+TransformComponent tc {11, 22, 10, 20, 5, 15, 2};
+
+TEST_CASE("Update dimensions changes the width and height") {
+    SDL_Rect r{};
+    tc.updateDimensions(r);
+
+    REQUIRE(r.w == 5);
+    REQUIRE(r.h == 15);
+}
+
+TEST_CASE("Update scaled dimensions changes the width and height by a factor") {
+    SDL_Rect r{};
+    tc.updateScaledDimensions(r);
+
+    REQUIRE(r.w == 10);
+    REQUIRE(r.h == 30);
+}
+
+TEST_CASE("Update position changes the x and y") {
+    SDL_Rect r{};
+    tc.updatePosition(r);
+
+    REQUIRE(r.x == 11);
+    REQUIRE(r.y == 22);
+}
+
+TEST_CASE("Updates a rectangle position given a factor") {
+    SDL_Rect r{};
+    tc.updatePositionByFactor(r, 10);
+
+    REQUIRE(r.y == 150);
+}
